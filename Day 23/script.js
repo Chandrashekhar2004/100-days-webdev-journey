a8f7e9e991f3db60b261ca86cf7b1b9c0c30d688
Chandrashekhar2004 <bhattchandu098@gmail.com>
2026-01-06 21:45:07 +0530

    Day 23 javascript question solution

---
+++ Day 23/script.js
@@ -0,0 +1,75 @@
+// Assignment 1
+// let num= prompt("Enter a number: ");
+
+// if(num % 10 === 0){
+//     console.log("Good ");
+// }
+// else{
+//     console.log("Bad ");
+// }
+
+// Assignment 2
+// let userName = prompt("Enter your name: ");
+// let age = prompt("Enter your age: ");
+
+// console.log(`Hello ${userName}, you are ${age} years old.`);
+
+// Assignment 3
+// let quarter = 2;
+
+// switch(quarter){
+//     case 1:
+//         console.log("January, February, March");    
+//         break;
+//     case 2:
+//         console.log("April, May, June");    
+//         break;  
+//     case 3:
+//         console.log("July, August, September");    
+//         break;
+//     case 4:
+//         console.log("October, November, December");    
+//         break;
+//     default:
+//         console.log("Invalid quarter");    
+// }
+
+// Assignment 4
+
+// let str = "Apples";
+
+// if(str[0] === 'A' || str[0] === 'a' && str.length >= 5){
+//     console.log("Golden string");
+// }
+// else{
+//     console.log("Not a golden string");
+// }
+
+// Assignment 5
+
+// let a = 50;
+// let b = 10;
+// let c = 15;
+
+// if(a > b && a > c){
+//     console.log("a is the greatest");
+
+// }
+// else if(b > a && b > c){
+//     console.log("b is the greatest");
+// }
+// else{
+//     console.log("c is the greatest");
+// }
+
+// Assignment 6
+
+// let num1 = prompt("Enter first number: ");
+// let num2 = prompt("Enter second number: ");
+
+// if((num1 % 10) == (num2 % 10)){
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
