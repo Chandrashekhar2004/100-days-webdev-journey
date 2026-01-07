76c8d68119b80d81f631a94234488155dfe6347b
Chandrashekhar2004 <bhattchandu098@gmail.com>
2026-01-08 01:17:15 +0530

    object literals in js

---
+++ Day 24/script.js
@@ -0,0 +1,36 @@
+// let student = {
+//     name: "chandrashekhar",
+//     age : 21,
+//     marks :80,
+//     city: "Nainital"
+// }
+
+// console.log(student.name);
+// console.log(student.age);
+
+// console.log(student.gender = "male");
+
+// // console.log( delete student.city);
+
+let studentInfo = {
+    chandrashekhar: {
+        age: 21,
+        marks: 80,
+    },
+    rahul: {
+        age: 22,
+        marks: 85,
+    },
+    ankit: {
+        age: 20,
+        marks: 78,
+    }
+}
+
+console.log(studentInfo.chandrashekhar.city = "Nainital");
+console.log(studentInfo.rahul.city = "Delhi");
+console.log(studentInfo.ankit.city = "Mumbai");
+
+console.log(studentInfo);
+
+// console.log(delete studentInfo.chandrashekhar.marks);
