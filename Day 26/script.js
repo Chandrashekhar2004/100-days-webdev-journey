150d42f2ac097b949549eca726737bd16a0833b3
Chandrashekhar2004 <bhattchandu098@gmail.com>
2026-01-08 19:43:51 +0530

    complete assignment questions.

---
+++ Day 26/script.js
@@ -0,0 +1,23 @@
+// qustion 1
+// let num = Math.floor(Math.random() * 6) + 1;
+// console.log("Random number between 1 and 6:", num);
+
+// question 2
+
+// let car = {
+//     name: "Toyota",
+//     model: "Corolla",
+//     color: "Blue",
+// };
+// console.log("Car Name:", car.name);
+// console.log("Car year :", car.year = 2020);
+
+// question 3
+
+let person = {
+    name : "chandrashekhar",
+    age: 22,
+    city: "Nainital"
+};
+console.log("Person new city :", person.city = "Delhi");
+console.log("Person object :", person);
